fc6a6b6b1881e1328fde7d9bb272d52baa0b68cc
TegridyInc <cassiemelink@gmail.com>
2025-05-15 15:44:52 +0200

    fixed property tree type states

--- src/scripts/Functions/PropertyTree.tsx
+++ src/scripts/Functions/PropertyTree.tsx
@@ -16,6 +16,8 @@ interface TypeData {
     type: number;
 }
 
+const typeState = new Map<number, Map<number, { isVisible: boolean, isHighlighted: boolean, highlightColor: THREE.Color }>>();
+
 export default function PropertyTree() {
     const [ifcModel, setIFCModel] = React.useState<IFCModel>();
 
@@ -34,7 +36,8 @@ export default function PropertyTree() {
 
             document.addEventListener('onModelRemoved', (e: CustomEvent<IFCModel>) =>{
                 setIFCModel(oldIFCModel => oldIFCModel == e.detail ? undefined : oldIFCModel)
-                    
+                
+                typeState.delete(e.detail.ifcID);
                 e.detail.dispatcher.removeEventListener('onPropertyTree', OpenPropertyTree)
             })
         }
@@ -69,6 +72,7 @@ function TypeFoldouts(props: { ifcModel: IFCModel }) {
             mounted.current = true;
             
             document.addEventListener('onModelAdded', (e: CustomEvent<IFCModel>) => {
+                typeState.set(e.detail.ifcID, new Map<number, { isVisible: boolean, isHighlighted: boolean, highlightColor: THREE.Color }>());
                 e.detail.dispatcher.addEventListener('onPropertyTree', UpdateItems)
             })
         }
@@ -100,6 +104,8 @@ function TypeFoldouts(props: { ifcModel: IFCModel }) {
     
                 if(index == -1) {
                     types.push({ data: [properties], objects: new Set<FRA.FragmentMesh>([child]), fragmentIDMap: null, type: properties.type });
+                    if(typeState.get(ifcModel.ifcID).get(properties.type) == undefined) 
+                        typeState.get(ifcModel.ifcID).set(properties.type, { isVisible: true, isHighlighted: false, highlightColor: new THREE.Color(1, 0, 0) });
                 } else {
                     if(!idsFound.has(id)) {
                         types[index].data.push(properties)
@@ -134,12 +140,12 @@ function TypeFoldouts(props: { ifcModel: IFCModel }) {
 }
 
 const TypeFoldout = (props: {typeData: TypeData, ifcModel: IFCModel}) => {
-    const [highlighted, setHighlight] = React.useState(false);
-    const [visible, setVisibilty] = React.useState(true);
+    const [highlighted, setHighlight] = React.useState(typeState.get(props.ifcModel.ifcID).get(props.typeData.type).isHighlighted);
+    const [visible, setVisibilty] = React.useState(typeState.get(props.ifcModel.ifcID).get(props.typeData.type).isVisible);
+    const [highlightColor, setHighlightColor] = React.useState( typeState.get(props.ifcModel.ifcID).get(props.typeData.type).highlightColor);
     const [isOpen, setOpenState] = React.useState(false);
 
     const name = webIFC.GetNameFromTypeCode(props.typeData.type);
-
     const mounted = React.useRef(false)
     React.useEffect(()=>{
         if(!mounted.current) {
@@ -165,22 +171,25 @@ const TypeFoldout = (props: {typeData: TypeData, ifcModel: IFCModel}) => {
         const color = highlighter.colors.get(name);
         if (color)
             color.set(rgb.r, rgb.g, rgb.b);
+
+        setHighlightColor(color);
+        typeState.get(props.ifcModel.ifcID).set(props.typeData.type, { isVisible: visible, isHighlighted: highlighted, highlightColor: color });
     }
 
     const toggleHighlight = (e:React.MouseEvent<HTMLElement>)=>{
-        setHighlight((oldValue)=>!oldValue);
-
-        const button = e.target as HTMLElement;
-        button.innerHTML = !highlighted ? 'lightbulb' : 'light_off'
+        setHighlight((oldValue)=>{
+            typeState.get(props.ifcModel.ifcID).set(props.typeData.type, { isVisible: visible, isHighlighted: !oldValue, highlightColor: highlightColor });
+            return !oldValue
+        });
 
         highlighter.highlightByID(name, highlighted ? {} : props.typeData.fragmentIDMap, true)
     }
 
     const toggleVisibility = (e:React.MouseEvent<HTMLElement>)=>{
-        setVisibilty((oldValue)=>!oldValue);
-
-        const button = e.target as HTMLElement;
-        button.innerHTML = !visible ? 'visibility' : 'visibility_off'
+        setVisibilty((oldValue)=>{
+            typeState.get(props.ifcModel.ifcID).set(props.typeData.type, { isVisible: !oldValue, isHighlighted: highlighted, highlightColor: highlightColor });
+            return !oldValue
+        });
 
         for (const threeObject of props.typeData.objects) {
             const colorMesh = culler.colorMeshes.get(threeObject.uuid);
@@ -202,13 +211,13 @@ const TypeFoldout = (props: {typeData: TypeData, ifcModel: IFCModel}) => {
         <FoldoutComponent sx={{border: '1px solid var(--highlight-color)'}} onClosed={async ()=> {setOpenState(false)}} onOpen={async ()=> {setOpenState(true)}} key={name} name={name} header={
             <Stack sx={{marginLeft: 'auto', alignItems: 'center', marginRight: '5px'}} spacing={.5} direction={'row'}>
                 <Tooltip title='Highlight Color'>
-                    <ColorInput type="color" className='color-input' defaultValue={'#ff0000'} onChange={changeHighlightColor}/>
+                    <ColorInput type="color" className='color-input' defaultValue={'#'+highlightColor.getHexString()} onChange={changeHighlightColor}/>
                 </Tooltip>
                 <Tooltip title='Highlight'>
-                    <ToggleButton value={highlighted} selected={highlighted} onClick={toggleHighlight}>light_off</ToggleButton>
+                    <ToggleButton value={highlighted} selected={highlighted} onClick={toggleHighlight}>{highlighted ? 'lightbulb' : 'light_off'}</ToggleButton>
                 </Tooltip>
                 <Tooltip title='Visibility'>
-                    <ToggleButton value={visible} selected={visible} onClick={toggleVisibility}>visibility</ToggleButton>
+                    <ToggleButton value={visible} selected={visible} onClick={toggleVisibility}>{visible ? 'visibility' : 'visibility_off'}</ToggleButton>
                 </Tooltip>
             </Stack>
         }>
